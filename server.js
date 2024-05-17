50682324dbd718da2127abe16fa71b090918432e
ando <ando@exponent.team>
2024-05-17 15:57:53 +0300

    first script

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const io = require("socket.io")(3000);
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: true, // true means to use any frontend.
+  },
+});
 
 io.on("connection", (socket) => {
-  socket.emit("chat-message", "bonjour");
+  socket.emit("chat-message", "bonjour le monde");
 });
